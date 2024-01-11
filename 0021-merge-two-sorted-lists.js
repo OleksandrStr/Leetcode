26b2e153dad79deba9d59bacbfd915fa7c91cf56
OleksandrStr <92752633+OleksandrStr@users.noreply.github.com>
2024-01-11 23:54:26 +0100

    Add recursive merge-two-sorted-lists js

--- 0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists.js
@@ -35,3 +35,24 @@ const mergeTwoLists = function (list1, list2) {
 
     return head.next;
 };
+
+/**
+ * Recursive
+ * Time O(N) | Space O(N)
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+const mergeTwoLists = function (list1, list2) {
+    if (!list1) {
+        return list2;
+    } else if (!list2) {
+        return list1;
+    } else if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+};
